24f5cda23e2d079154fd69599e4fca9c1717b1b6
truongthieuhuyen <nguyentientruong97@gmail.com>
2022-02-15 18:35:58 +0700

    fisrt upload [profile, edit profile, payment method, order, news, FAQs ]

---
+++ editProfile.js
@@ -0,0 +1,28 @@
+/* Handler see password */
+var state = false;
+function toggle() {
+	if (state) {
+		document.getElementById("pass1").setAttribute("type", "password");
+		document.getElementById("img1").src = "Images/Symbols/blindfold.png"
+		state = false
+	} else {
+		document.getElementById("pass1").setAttribute("type", "text");
+		document.getElementById("img1").src = "Images/Symbols/Read.png"
+		state = true;
+	}
+}
+
+/* Change Password */
+function save() {
+	alert("Password has changed");
+}
+/* Sign out */
+
+
+
+// $('#exampleModal').on('show.bs.modal', event => {
+// 	var button = $(event.relatedTarget);
+// 	var modal = $(this);
+// 	// Use above variables to manipulate the DOM
+
+// });
